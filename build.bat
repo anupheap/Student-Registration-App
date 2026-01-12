ecd687e38be335cca8c7eec77b4536f715a7dda6
solaris✨ <phearakleapung@gmail.com>
2026-01-12 08:31:39 +0700

    Removed the code block that excludes the OpenGL dll since the flag already exists

--- build.bat
+++ build.bat
@@ -3,27 +3,28 @@ echo ------------------------------------------------
 echo   Building Student Grouping App (Standalone)
 echo ------------------------------------------------
 
-:: 1. Create output folder if missing
+:: Creates Output Folder if it doesn't exist
 if not exist output mkdir output
 
-:: 2. Copy VM Graphics Fix (if present)
-if exist output/opengl32.dll (
-    copy /Y opengl32.dll output\ >nul
-    echo [OK] OpenGL Fix copied.
+:: Makes sure opengl32.dll is in the output folder
+if exist output\opengl32.dll (
+    copy /Y opengl32.dll output\ > nul
+    echo [OKAY] Copied opengl32.dll to output folder.
 )
-if not exist output/raylib.dll (
-    echo [WARN] raylib.dll not yet found! Pulling it from the source file...
+
+:: Pull in raylib.dll if not already present
+if not exist output\raylib.dll (
+    echo [WARNING] raylib.dll not yet found! Pulling it from the source file...
     copy /Y raylib.dll output\
 ) else (
-    echo [OK] raylib.dll found.
+    echo [OKAY] raylib.dll found.
 )
-:: 3. Compile
-:: -static : Bakes libraries into the exe so it runs anywhere
-:: -mwindows : Hides the black console window
+
+::Compiles the code
 echo Compiling...
 g++ main.cpp -o output/main.exe -O2 -I include -L lib -lraylib -lopengl32 -lgdi32 -lwinmm -static -mwindows
 
-:: 4. Check for errors
+:: Error Detection
 if %errorlevel% neq 0 (
     echo.
     echo [ERROR] Build Failed! Check your code.
@@ -34,5 +35,4 @@ if %errorlevel% neq 0 (
 echo [SUCCESS] Running Application...
 echo.
 
-:: 5. Run it
 output\main.exe
