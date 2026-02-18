9cf97d1c5239ab949fc9ad94631df15384815694
solaris✨ <phearakleapung@gmail.com>
2026-02-18 17:03:40 +0700

    resync again

--- src/mainMenuElements.hpp
+++ src/mainMenuElements.hpp
@@ -8,8 +8,9 @@
 #include "reasings.h"
 #include "buttonsAndTextFields.hpp"
 #include "nlohmann/json.hpp"
+#include <fstream>
 
-
+RegistrationToggles registrations;
 class Bars{
     private:
         Animations animBars;
@@ -167,5 +168,105 @@ inline void Panels::Draw(float xpos, float ypos, originDirection selectedDirecti
     Vector2 currentPos = { originPos.x + offsetX, originPos.y + offsetY };
 
     DrawTextureEx(registrationPanel, currentPos, 0, 1, WHITE);
+}
+
+void safePush(RegistrationToggles unitSelect, int groupingNumber, bool isSelectAll){
+    extern ordered_json data;
+    const char* registrationData[] = {"Registrations for 1E1", "Registrations for 1E2", "Registrations for 1E3", "Registrations for 1E4"};
+    const char* units[] = {"Programming", "Physics I", "Mathematics", "Writing And Researching Skills"};
+    if(isSelectAll){
+        for(auto& unitSelection : units){
+            for (auto& [key, value] : data.items()){
+                if(key == registrationData[groupingNumber]) continue;
+                for(auto& arr : value){
+                    if(arr == unitSelection) TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", unitSelection, key.c_str());
+                }
+            }
+            data[registrationData[groupingNumber]].push_back(unitSelection);
+        }
+    }else{
+        for (auto& [key, value] : data.items()){
+            if(key == registrationData[groupingNumber]){
+                for(auto& arr : value){
+                    if(arr == units[i]) TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", units[i], key.c_str());
+                }
+                continue;
+            }
+            for(auto& arr : value){
+                if(arr == units[i]) TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", units[i], key.c_str());
+            }
+
+        }
+        data[registrationData[groupingNumber]].push_back(units[i]);
+    }
+}
+void safePush(RegistrationToggles unitSelect){
+    
+}
+
+void setRegistration(int groupingNumber){
+    extern ordered_json data;
+    const char* registrationData[] = {"Registrations for 1E1", "Registrations for 1E2", "Registrations for 1E3", "Registrations for 1E4"};
+    const char* units[] = {"Programming", "Physics I", "Mathematics", "Writing And Researching Skills"};
+    if(registrations.toggleStateForGroupings[groupingNumber]){
+        for (int i = 0; i < 5; i++){
+            if(registrations.toggleStateForUnits[4]){
+                for(auto& unitSelection : units){
+                    for (auto& [key, value] : data.items()){
+                        if(key == registrationData[groupingNumber]) continue;
+                        for(auto& arr : value){
+                            if(arr == unitSelection) TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", unitSelection, key.c_str());
+                        }
+                    }
+                    data[registrationData[groupingNumber]].push_back(unitSelection);
+                }
+                break;
+            }
+            if(registrations.toggleStateForUnits[i]){
+                for (auto& [key, value] : data.items()){
+                    if(key == registrationData[groupingNumber]){
+                        for(auto& arr : value){
+                            if(arr == units[i]) TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", units[i], key.c_str());
+                        }
+                        continue;
+                    }
+                    for(auto& arr : value){
+                        if(arr == units[i]) TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", units[i], key.c_str());
+                    }
+
+                }
+                data[registrationData[groupingNumber]].push_back(units[i]);
+            }
+        }
+    }
+}
+void setStudentInfo(){
+    extern ordered_json data;
+    data["name"] = info.studentName;
+    data["id"] = info.studentID;
+    data["semester"] = info.studentSemester;
+    data["year"] = info.studentYear;
+}
+void writeIntoJson(){
+    extern ordered_json data;
+    extern StudentInfo info;
+    ofstream outDocs(info.studentFileName);
+    if(outDocs.is_open()){
+        outDocs << data.dump(8);
+        outDocs.close();
+    }
+}
+void primeJsonFile(){
+    extern ordered_json data;
+    extern StudentInfo info;
+    ifstream inDocs(info.studentFileName);
+    if(!inDocs.is_open()){
+        ofstream outDocs(info.studentFileName);
+        outDocs << data.dump(8);
+        outDocs.close();
+    } else{
+        inDocs >> data;
+    }
+    inDocs.close();
 }
 #endif
