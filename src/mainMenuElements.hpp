ce882ab7caccc05c1b47f44edac8970479c92a9e
solaris✨ <phearakleapung@gmail.com>
2026-03-17 10:34:57 +0700

    improved the slot decrease code

--- src/mainMenuElements.hpp
+++ src/mainMenuElements.hpp
@@ -9,21 +9,84 @@
 #include "buttonsAndTextFields.hpp"
 #include "nlohmann/json.hpp"
 #include <fstream>
+#include <filesystem>
 #include "student.h"
 
-void availibilityDecrease(const char* selectedUnit);
+inline std::string getBaseDir(){
+    return std::string(GetApplicationDirectory());
+}
+
+inline std::string buildPath(const char* relative){
+    return getBaseDir() + relative;
+}
+
+inline void recalculateSlots(int year){
+    const char* units[] = {"Programming", "Physics I", "Mathematics II", "Writing And Researching Skills"};
+    const int MAX_SLOTS = 15;
+
+    ordered_json availableSlots;
+    for(auto& u : units) availableSlots[u] = MAX_SLOTS;
+
+    std::string studentsDir = buildPath("records/students/");
+    std::filesystem::create_directories(studentsDir);
+
+    for(const auto& entry : std::filesystem::directory_iterator(studentsDir)){
+        if(entry.path().extension() != ".json") continue;
+
+        std::ifstream f(entry.path());
+        if(!f.is_open()) continue;
+
+        ordered_json studentData;
+        try{ 
+            f >> studentData;
+        }catch(...){
+            continue;
+        }
+
+        if(!studentData.contains("year") || studentData["year"] != year) continue;
+
+        const char* groups[] = {"Registrations for 1E1", "Registrations for 1E2", "Registrations for 1E3", "Registrations for 1E4"};
+        for(auto& group : groups){
+            if(!studentData.contains(group)) continue;
+            for(auto& registeredUnit : studentData[group]){
+                std::string unitStr = registeredUnit.get<std::string>();
+                if(availableSlots.contains(unitStr)){
+                    int current = availableSlots[unitStr].get<int>();
+                    if(current > 0) availableSlots[unitStr] = current - 1;
+                }
+            }
+        }
+    }
+
+    char yearFileName[64];
+    sprintf(yearFileName, "year%d.json", year);
+    std::string slotDir  = buildPath("records/availableSlots/");
+    std::string slotPath = slotDir + yearFileName;
+    std::filesystem::create_directories(slotDir);
+
+    {
+        std::ofstream out(slotPath);
+        if(out.is_open()){
+            out << availableSlots.dump(4);
+            TraceLog(LOG_INFO, "Slots recalculated and saved: %s", slotPath.c_str());
+        } else {
+            TraceLog(LOG_ERROR, "FAILED TO WRITE SLOT FILE: %s", slotPath.c_str());
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------
 
 RegistrationToggles registrations;
+
 class Bars{
     private:
         Animations animBars;
         Color selectedColor;
-
         char displayText[200];
         char displayName[200];
         float currentYScale = 0.0f;
-        float finalYScale = 99.0f;
-        
+        float finalYScale   = 99.0f;
     public:
         Bars(Color color);
         ~Bars();
@@ -40,55 +103,45 @@ class Panels{
         Panels(char* panelTexturePath);
         ~Panels();
         void Draw(float xpos, float ypos, originDirection selectedDirection, int toggleNumber);
-
 };
 
 class CheckBoxesForGroupings{
-
     public:
         CheckBoxesForGroupings() = default;
         bool Draw(float xpos, float ypos, const char* text, int toggleLabel, bool* toggleStateRadio, textFonts &font){
             Rectangle collisionBoxForCheckBoxes = {
-                xpos,
-                ypos,
-                45 + MeasureTextEx(font.torus30, text, 30, 3).x,
-                30
+                xpos, ypos,
+                45 + MeasureTextEx(font.torus30, text, 30, 3).x, 30
             };
             Color colorState;
             Color mmYesGREEEEEEN = {172, 247, 98, 255};
             bool isHovering = CheckCollisionPointRec(GetMousePosition(), collisionBoxForCheckBoxes);
             bool clicked = (isHovering && IsMouseButtonPressed(MOUSE_BUTTON_LEFT));
             if(clicked){
-                if (toggleStateRadio[toggleLabel]){
+                if(toggleStateRadio[toggleLabel]){
                     toggleStateRadio[toggleLabel] = !toggleStateRadio[toggleLabel];
-                }else{
-                    for (int i = 0; i < 4; i++){
-                        toggleStateRadio[i] = (i == toggleLabel);
-                    }
+                } else {
+                    for(int i = 0; i < 4; i++) toggleStateRadio[i] = (i == toggleLabel);
                 }
-            } else if (isHovering || toggleStateRadio[toggleLabel]){
+            } else if(isHovering || toggleStateRadio[toggleLabel]){
                 colorState = mmYesGREEEEEEN;
-            } else{
+            } else {
                 colorState = WHITE;
             }
             DrawRectangleLines(xpos, ypos, 30, 30, colorState);
             DrawTextEx(font.torus30, text, {xpos + 40, ypos}, 30, 3, colorState);
-            if(toggleStateRadio[toggleLabel]){
-                DrawRectangle(xpos+5, ypos+5, 20, 20, colorState);
-            } 
+            if(toggleStateRadio[toggleLabel]) DrawRectangle(xpos + 5, ypos + 5, 20, 20, colorState);
             return toggleStateRadio[toggleLabel];
         }
 };
-class CheckBoxesForUnits{
 
+class CheckBoxesForUnits{
     public:
         CheckBoxesForUnits() = default;
         bool Draw(float xpos, float ypos, const char* text, int toggleLabel, bool* toggleStateRadio, textFonts &font){
             Rectangle collisionBoxForCheckBoxes = {
-                xpos,
-                ypos,
-                45 + MeasureTextEx(font.torus30, text, 30, 3).x,
-                30
+                xpos, ypos,
+                45 + MeasureTextEx(font.torus30, text, 30, 3).x, 30
             };
             Color colorState;
             Color mmYesGREEEEEEN = {172, 247, 98, 255};
@@ -97,25 +150,21 @@ class CheckBoxesForUnits{
             if(clicked){
                 toggleStateRadio[toggleLabel] = !toggleStateRadio[toggleLabel];
                 if(toggleStateRadio[4]){
-                    for (int i = 0; i < 5; i++) toggleStateRadio[i] = (i == toggleLabel);
+                    for(int i = 0; i < 5; i++) toggleStateRadio[i] = (i == toggleLabel);
                 }
-            } else if (isHovering || toggleStateRadio[toggleLabel]){
+            } else if(isHovering || toggleStateRadio[toggleLabel]){
                 colorState = mmYesGREEEEEEN;
-            } else{
+            } else {
                 colorState = WHITE;
             }
             DrawRectangleLines(xpos, ypos, 30, 30, colorState);
             DrawTextEx(font.torus30, text, {xpos + 40, ypos}, 30, 3, colorState);
-            if(toggleStateRadio[toggleLabel]){
-                DrawRectangle(xpos+5, ypos+5, 20, 20, colorState);
-            } 
+            if(toggleStateRadio[toggleLabel]) DrawRectangle(xpos+5, ypos+5, 20, 20, colorState);
             return toggleStateRadio[toggleLabel];
         }
 };
 
-inline Bars::Bars(Color color){
-    selectedColor = color;
-}
+inline Bars::Bars(Color color){ selectedColor = color; }
 inline void Bars::Draw(int toggleNumber){
     float deltaTime = GetFrameTime();
     animBars.animDuration = 0.15f;
@@ -124,25 +173,21 @@ inline void Bars::Draw(int toggleNumber){
     if(animBars.animTimer > animBars.animDuration) animBars.animTimer = animBars.animDuration;
     else if(animBars.animTimer < 0) animBars.animTimer = 0;
     currentYScale = EaseBackOut(animBars.animTimer, 0.0f, finalYScale, animBars.animDuration);
-    DrawRectangle(0, 99, GetScreenWidth(), currentYScale, selectedColor);  
+    DrawRectangle(0, 99, GetScreenWidth(), currentYScale, selectedColor);
 }
-inline Bars::~Bars(){
-    animBars.animTimer = 0.0f;
-    currentYScale = 0.0f;
-}
+inline Bars::~Bars(){ animBars.animTimer = 0.0f; currentYScale = 0.0f; }
 
-inline Panels::Panels(char* panelTexturePath)
-{
+inline Panels::Panels(char* panelTexturePath){
     Image mainMenuRegistrationPanel = LoadImage(panelTexturePath);
-    ImageResize(&mainMenuRegistrationPanel, (mainMenuRegistrationPanel.width/2) - 70, (mainMenuRegistrationPanel.height/2) - 70);
+    ImageResize(&mainMenuRegistrationPanel,
+        (mainMenuRegistrationPanel.width/2)  - 70,
+        (mainMenuRegistrationPanel.height/2) - 70);
     registrationPanel = LoadTextureFromImage(mainMenuRegistrationPanel);
     GenTextureMipmaps(&registrationPanel);
     SetTextureFilter(registrationPanel, TEXTURE_FILTER_TRILINEAR);
     UnloadImage(mainMenuRegistrationPanel);
 }
-inline Panels::~Panels(){
-    UnloadTexture(registrationPanel);
-}
+inline Panels::~Panels(){ UnloadTexture(registrationPanel); }
 inline void Panels::Draw(float xpos, float ypos, originDirection selectedDirection, int toggleNumber){
     float deltaTime = GetFrameTime();
     animPanels.animDuration = 0.2f;
@@ -152,81 +197,70 @@ inline void Panels::Draw(float xpos, float ypos, originDirection selectedDirecti
             break;
         }
         case (TOPRIGHT):{
-            originPos = {(float)(GetScreenWidth() - registrationPanel.width), (float)(0-registrationPanel.height)};
+            originPos = {(float)(GetScreenWidth() - registrationPanel.width), (float)(0 - registrationPanel.height)};
+            break;
+        }
+        default:{
             break;
         }
     }
-
     if(toggleState[toggleNumber]) animPanels.animTimer += deltaTime;
     else animPanels.animTimer -= deltaTime;
     if(animPanels.animTimer > animPanels.animDuration) animPanels.animTimer = animPanels.animDuration;
     else if(animPanels.animTimer < 0.0f) animPanels.animTimer = 0.0f;
 
-    float deltaX = xpos - originPos.x;
-    float deltaY = ypos - originPos.y;
-
+    float deltaX  = xpos - originPos.x;
+    float deltaY  = ypos - originPos.y;
     float offsetX = EaseExpoOut(animPanels.animTimer, 0, deltaX, animPanels.animDuration);
     float offsetY = EaseExpoOut(animPanels.animTimer, 0, deltaY, animPanels.animDuration);
-
-    Vector2 currentPos = { originPos.x + offsetX, originPos.y + offsetY };
-
-    DrawTextureEx(registrationPanel, currentPos, 0, 1, WHITE);
+    DrawTextureEx(registrationPanel, {originPos.x + offsetX, originPos.y + offsetY}, 0, 1, WHITE);
 }
 
 void setRegistration(int groupingNumber){
     extern ordered_json data;
     const char* registrationData[] = {"Registrations for 1E1", "Registrations for 1E2", "Registrations for 1E3", "Registrations for 1E4"};
     const char* units[] = {"Programming", "Physics I", "Mathematics II", "Writing And Researching Skills"};
-    bool containsDuplicates = false;
-    if(registrations.toggleStateForGroupings[groupingNumber]){
-        for (int i = 0; i < 5; i++){
-            if(registrations.toggleStateForUnits[4]){
-                for(auto& unitSelection : units){
-                    for (auto& [key, value] : data.items()){
-                        for(auto& arr : value){
-                            if(arr == unitSelection){
-                                TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", unitSelection, key.c_str());
-                                containsDuplicates = true;
-                            }
-                        }
-                    }
-                    if(!containsDuplicates){
-                        data[registrationData[groupingNumber]].push_back(unitSelection);
-                        availibilityDecrease(unitSelection);
-                        TraceLog(LOG_INFO, "\"%s\" Added to group \"%s\"", unitSelection, registrationData[groupingNumber]);
-                    }
-                    else return;
-                }
-                break;
-            }
-            if(registrations.toggleStateForUnits[i]){
-                for (auto& [key, value] : data.items()){
-                    if(key == registrationData[groupingNumber]){
-                        for(auto& arr : value){
-                            if(arr == units[i]) {
-                                TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", units[i], key.c_str());
-                                containsDuplicates = true;
-                            }
-                        }
-                        continue;
-                    }
+
+    if(!registrations.toggleStateForGroupings[groupingNumber]) return;
+
+    for(int i = 0; i < 5; i++){
+        if(registrations.toggleStateForUnits[4]){
+            for(auto& unitSelection : units){
+                bool duplicate = false;
+                for(auto& [key, value] : data.items()){
                     for(auto& arr : value){
-                        if(arr == units[i]){
-                            TraceLog(LOG_WARNING, "\"%s\" Already exists in group \"%s\"", units[i], key.c_str());
-                            containsDuplicates = true;
+                        if(arr == unitSelection){
+                            TraceLog(LOG_WARNING, "\"%s\" already in \"%s\"", unitSelection, key.c_str());
+                            duplicate = true;
                         }
                     }
                 }
-                if(!containsDuplicates){
-                    data[registrationData[groupingNumber]].push_back(units[i]);
-                    availibilityDecrease(units[i]);
-                    TraceLog(LOG_INFO, "\"%s\" Added to group \"%s\"", units[i], registrationData[groupingNumber]);
+                if(!duplicate){
+                    data[registrationData[groupingNumber]].push_back(unitSelection);
+                    TraceLog(LOG_INFO, "\"%s\" added to \"%s\"", unitSelection, registrationData[groupingNumber]);
                 }
-                else return;
             }
+            break;
+        }
+
+        if(!registrations.toggleStateForUnits[i]) continue;
+
+        bool duplicate = false;
+        for(auto& [key, value] : data.items()){
+            for(auto& arr : value){
+                if(arr == units[i]){
+                    TraceLog(LOG_WARNING, "\"%s\" already in \"%s\"", units[i], key.c_str());
+                    duplicate = true;
+                }
+            }
+        }
+        if(!duplicate){
+            data[registrationData[groupingNumber]].push_back(units[i]);
+            TraceLog(LOG_INFO, "\"%s\" added to \"%s\"", units[i], registrationData[groupingNumber]);
         }
     }
 }
+
 void setStudentInfo(){
     extern ordered_json data;
     data["name"] = info.studentName;
@@ -234,73 +268,49 @@ void setStudentInfo(){
     data["semester"] = info.studentSemester;
     data["year"] = info.studentYear;
 }
+
 void writeIntoJson(){
     extern ordered_json data;
-    extern StudentInfo info;
-    ofstream outDocs(info.studentFileName);
-    if(outDocs.is_open()){
-        outDocs << data.dump(8);
-        outDocs.close();
+
+    std::string studentsDir = buildPath("records/students/");
+    std::filesystem::create_directories(studentsDir);
+
+    std::string fullPath = buildPath(info.studentFileName);
+
+    {
+        std::ofstream outDocs(fullPath);
+        if(outDocs.is_open()){
+            outDocs << data.dump(8);
+            TraceLog(LOG_INFO, "Student file written: %s", fullPath.c_str());
+        } else {
+            TraceLog(LOG_ERROR, "FAILED TO WRITE STUDENT FILE: %s", fullPath.c_str());
+            return;
+        }
     }
-}
-void primeJsonFile(){
-    extern ordered_json data;
-    extern StudentInfo info;
-    ifstream inDocs(info.studentFileName);
-    if(!inDocs.is_open()){
-        ofstream outDocs(info.studentFileName);
-        outDocs << data.dump(8);
-        outDocs.close();
-    } else{
-        inDocs >> data;
-    }
-    inDocs.close();
+
+    recalculateSlots(info.studentYear);
 }
 
-inline void availibilityDecrease(const char* selectedUnit){
+void primeJsonFile(){
     extern ordered_json data;
-    const char* years[7] = {"year2024.json", "year2025.json", "year2026.json", "year2027.json", "year2028.json", "year2029.json", "year2030.json"};
-    char filePath[500] = "records/availableSlots/";
-    if(info.studentYear == 2024) strcat(filePath, years[0]);
-    
-    TraceLog(LOG_INFO, filePath);
-    ordered_json availableSlots;
-    ifstream availableSlotsIn(filePath);
-    
-    if(!availableSlotsIn.is_open()){
-        availableSlots = {
-            {"Programming", 15},
-            {"Physics I", 15},
-            {"Mathematics II", 15},
-            {"Writing and Researching Skills", 15}
-        };
-        ofstream slotFileOut(filePath);
-        slotFileOut << availableSlots.dump(4);
-        slotFileOut.close();
-    } else{
-        availableSlotsIn >> availableSlots;
-        TraceLog(LOG_INFO, "Copying Data");
-    }
-    availableSlotsIn.close();
-    if (availableSlots[selectedUnit].is_number_integer()) {
-        availableSlots[selectedUnit] = availableSlots[selectedUnit].get<int>() - 1;
-        if(availableSlots[selectedUnit] == 14) TraceLog(LOG_INFO, "Deducted");
-        TraceLog(LOG_INFO, filePath);
-    }
-   // availableSlots[selectedUnit] = availableSlots[selectedUnit].get<int>() - 1;
-    /*
-    if (availableSlots.contains(selectedUnit) && availableSlots[selectedUnit].is_number()) {
-        availableSlots[selectedUnit] = availableSlots[selectedUnit].get<int>() - 1;
-    }
-        */
-    ofstream availableSlotsOut(filePath);
-    if(!availableSlotsOut.is_open()){
-        TraceLog(LOG_ERROR, "FAILED TO OPEN FILE");
-    }
-    availableSlotsOut << availableSlots.dump(4);
-    availableSlotsOut.close();
-    if(availableSlotsOut.fail()){
-        TraceLog(LOG_ERROR, "FAILED TO WRITE INTO FILE");
+
+    std::string studentsDir = buildPath("records/students/");
+    std::filesystem::create_directories(studentsDir);
+
+    std::string fullPath = buildPath(info.studentFileName);
+
+    std::ifstream inDocs(fullPath);
+    if(!inDocs.is_open()){
+        std::ofstream outDocs(fullPath);
+        if(outDocs.is_open()){
+            outDocs << data.dump(8);
+            TraceLog(LOG_INFO, "New student file created: %s", fullPath.c_str());
+        } else {
+            TraceLog(LOG_ERROR, "FAILED TO CREATE STUDENT FILE: %s", fullPath.c_str());
+        }
+    } else {
+        inDocs >> data;
+        TraceLog(LOG_INFO, "Existing student file loaded: %s", fullPath.c_str());
     }
 }
 
